83736a8bc2c6ec3f28f4c5c9903e3cf59b2b7ab6
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-25 17:36:08 +0200

    Step 4 - Hello, wolrd!

---
+++ index.jsx
@@ -0,0 +1,9 @@
+// #1. Create the entry point for the React application
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Returns "Hello, world!"
+ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('app')
+);
