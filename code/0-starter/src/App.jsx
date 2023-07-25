152bba1089cad2c5657065302af22e042b35cc24
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-25 17:53:43 +0200

    Step 5

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,12 @@
+// #1. Create the component step
+import React from 'react';
+
+function App() {
+    return (
+        <article>
+            <h1>Recipe Manager</h1>
+        </article>
+    )
+}
+
+export default App;
