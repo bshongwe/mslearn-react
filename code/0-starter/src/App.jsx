ca0fd53c089c6e903ac9280c9e1088ed49d1376d
bshongwe <dru.da.beat.hustle@gmail.com>
2023-07-29 11:12:56 +0200

    Unit 7

--- code/0-starter/src/App.jsx
+++ code/0-starter/src/App.jsx
@@ -1,3 +1,5 @@
+// Unit 7 - Import the CSS - Step 2
+import './index.css'
 // #1. Create the component step
 import React from 'react';
 import RecipeTitle from './RecipeTitle' // Use the RecipeTitle component - Step 6
@@ -6,6 +8,7 @@ function App() {
     return (
         <article>
             <h1>Recipe Manager</h1>
+            <RecipeTitle /> // Use the RecipeTitle component - Step 3
         </article>
     )
 }
